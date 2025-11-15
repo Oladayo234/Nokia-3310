f643397d46cb4126a1abbfa453551536f705d762
Oladayo234 <oladayoolubunmi20@gmail.com>
2025-11-15 17:10:47 +0100

    second commit

--- Nokia.java
+++ Nokia.java
@@ -11,7 +11,8 @@ public class Nokia{
      Scanner input = new Scanner(System.in);
      System.out.println("""
 
-    
+        NOKIA MENU
+        ---------------------------
         1. Phonebook
         2. Messages
         3. Chats
@@ -25,17 +26,16 @@ public class Nokia{
         11. Clock
         12. Profiles
         13. SIM services
-
+        ----------------------------
+    Enter a number to navigate 
 """); 
        
-
-
-
-    System.out.println("Chooose an option ");
+//    System.out.println("Chooose an option ");
     int phoneBook = input.nextInt();
 
     switch (phoneBook){
-    case 1-> {System.out.println("""
+    case 1-> {
+    System.out.println("""
 
     1. Search
     2. Service Nos
@@ -46,21 +46,18 @@ public class Nokia{
     7. Send b'card
     8. Options
     9. Speed dials
-    10. VOice Tags
+    10. Voice Tags
 
 """);
 
  
 
-
-
-
-
-    System.out.println("Choose an option ");
-    int optionEight = input.nextInt();
+//    System.out.println("Choose an option ");
+        int optionEight = input.nextInt();
 
     switch (optionEight){
-    case 8-> System.out.println("""
+    case 8-> 
+    System.out.println("""
 
     1. Type of view
     2. Memory Status
@@ -71,15 +68,13 @@ public class Nokia{
      }
 }
 
-
-
-
-
-    System.out.print("Choose an option");
-    int message = input.nextInt();
+    
+//    System.out.print("Choose an option");
+        int message = input.nextInt();
 
     switch (message){
-    case 2-> {System.out.println("""
+    case 2-> {
+    System.out.println("""
 
     1. Write message
     2. Inbox
@@ -96,8 +91,8 @@ public class Nokia{
 
 
 
-    System.out.println("Choose an option");
-    int messageSettings = input.nextInt();
+//    System.out.println("Choose an option");
+        int messageSettings = input.nextInt();
 
     switch (messageSettings){
     case 7-> {System.out.println("""
@@ -108,8 +103,8 @@ public class Nokia{
 """);
      
 
-    System.out.println("Choose an option");
-    int set = input.nextInt();
+//    System.out.println("Choose an option");
+        int set = input.nextInt();
 
     switch (set){
     case 1-> System.out.println("""
@@ -119,11 +114,11 @@ public class Nokia{
     3. Message validity
 
 """);
-        }
+     }
 
 
-    System.out.println("Choose an option");
-    int common = input.nextInt();
+//    System.out.println("Choose an option");
+        int common = input.nextInt();
 
     switch (common){
     case 2-> System.out.println("""
@@ -133,143 +128,302 @@ public class Nokia{
     3. Character Support
 
 """);
-        }
+       } 
     }
     }
   }
 }
 
-//    System.out.print("Chooose an option");
-//    int chat = input.nextInt();
-//
-//    switch (chat){
-//    case 3-> System.out.println("""
-//
-//    1. Chat
-//
-//""");
-//
-//     }
-//    
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//     System.out.print("Chooose an option");
-//        int callRegister = input.nextInt();
-//
-//    switch (callRegister){
-//    case 4-> System.out.println("""
-//
-//    1. Missed calls
-//    2. Recieved calls
-//    3. Dialled numbers
-//    4. Erase recent call lists
-//    5. Show call duration
-//    6. Show call cost
-//    7. Call cost settings
-//    8. Prepaid credit
-//
-//""");
-//
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    System.out.print("Choose an option");
-//    int ShowCallDuration = input.nextInt();
-//
-//    switch (ShowCallDuration){
-//    case 5-> System.out.println("""
-//
-//    1. Last call duration
-//    2. All calls' duration
-//    3. Recieve calls' duration
-//    4. Dialled calls' duration
-//    5. Clear timers
-//
-//""");
-//     }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    System.out.print("Choose an option");
-//    int ShowCallCost = input.nextInt();
-//
-//    switch (ShowCallCost){
-//    case 5-> System.out.println("""
-//
-//    1. Last call cost
-//    2. All call's cost
-//    3. clear counter
-//
-//""");
-//     }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    System.out.print("Choose an option");
-//    int callCostSettings = input.nextInt();
-//
-//    switch (callCostSettings){
-//    case 5-> System.out.println("""
-//
-//    1. Call cost limit
-//    2. show costs in
-//
-//""");
-//     }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+//    System.out.println("Chooose an option");
+        int chat = input.nextInt();
+
+    switch (chat){
+    case 3-> System.out.println("""
+
+    1. Chat
+
+""");   
+     }
 
 
+//     System.out.println("Chooose an option");
+        int callRegister = input.nextInt();
+
+    switch (callRegister){
+    case 4-> {System.out.println("""
+
+    1. Missed calls
+    2. Recieved calls
+    3. Dialled numbers
+    4. Erase recent call lists
+    5. Show call duration
+    6. Show call cost
+    7. Call cost settings
+    8. Prepaid credit
+
+""");
+
+
+//    System.out.println("Choose an option");
+        int missedCalls = input.nextInt();
+
+    switch (missedCalls){
+    case 1-> System.out.println("""
+    1. Missed calls
+
+""");
+    }
+
+//    System.out.println("Choose an option");
+        int recievedCalls = input.nextInt();
+
+    switch (recievedCalls){
+    case 2-> System.out.println("""
+    1. Recieved calls
+
+""");
+    }
+
+//    System.out.println("Choose an option");
+        int dialledNumber = input.nextInt();
+
+    switch (dialledNumber){
+    case 3-> System.out.println("""
+    1. Dialled number
+
+""");
+    
+    }
+//    System.out.println("Choose an option");
+        int eraserecent = input.nextInt();
+
+    switch (eraserecent){
+    case 4-> System.out.println("""
+    1. Erase recent
+
+""");
+    }
+
+//    System.out.println("Choose an option");
+        int ShowCallDuration = input.nextInt();
+
+    switch (ShowCallDuration){
+    case 5-> System.out.println("""
+
+    1. Last call duration
+    2. All calls' duration
+    3. Recieve calls' duration
+    4. Dialled calls' duration
+    5. Clear timers
+
+""");
+    }
+     
+
+//    System.out.println("Choose an option");
+        int ShowCallCost = input.nextInt();
+
+    switch (ShowCallCost){
+    case 6-> System.out.println("""
+
+    1. Last call cost
+    2. All call's cost
+    3. clear counter
+
+""");
+    }
+      
+//    System.out.println("Choose an option");
+        int callCostSettings = input.nextInt();
+
+    switch (callCostSettings){
+    case 7-> System.out.println("""
+
+    1. Call cost limit
+    2. show costs in
+
+""");
+    }
+
+//    System.out.println("Choose an option");
+        int prepaid = input.nextInt();
+
+    switch (prepaid){
+    case 8-> System.out.println("""
+    1. Erase recent
+
+""");
+    }
+    }
+    }
+
+//    System.out.print("Choose an option");
+        int tones = input.nextInt();
+
+    switch (tones){
+    case 5-> System.out.println("""
+
+    1. Ringing tones
+    2. Ringing Volumes
+    3. Incoming call alert
+    4. Composer
+    5. Message alert tones
+    6. Keypad tones
+    7. Warning and game tones
+    8. vibrating alert
+    9. Screen saver
+
+""");
+    }
+
+
+//     System.out.println("Chooose an option");
+        int settings = input.nextInt();
+
+    switch (settings){
+    case 6-> {System.out.println("""
+
+    1. Call Settings
+    2. Phone Setting
+    3. Security settings
+    4. Restore Factory settings
+    
+""");
+
+//    System.out.println("Choose an option");
+        int callSetting = input.nextInt();
+
+    switch (callSetting){
+    case 1-> System.out.println("""
+    1. Automatic redial
+    2. Speed dialling
+    3. Call waiting options
+    4. Own number sending
+    5. Phone line in use
+    6. Automatic answer
+
+"""); 
+    }
+
+//    System.out.println("Choose an option");
+        int phoneSettings = input.nextInt();
+
+    switch (phoneSettings){
+    case 2-> System.out.println("""
+    1. Language
+    2. Cell info display
+    3. Welcome note
+    4. Network selection
+    5. Lights
+    6. Confirm SIM service actions
+
+
+""");
+    }
+
+//    System.out.println("Choose an option");
+        int securitySettings = input.nextInt();
+
+    switch (securitySettings){
+    case 3-> System.out.println("""
+    1. PIN code request
+    2. call barring service
+    3. Fixed dialling
+    4. Closed user group
+    5. Phone security
+    6. Change access codes
+
+""");
+    }
+
+//    System.out.println("Choose an option");
+         int restoreFactorySettings = input.nextInt();
+
+    switch (restoreFactorySettings){
+    case 4-> System.out.println("""
+    1. Restore factory settings
+
+""");
+   }
+   }
+   }
+
+
+    
+//    System.out.println("Choose an option");
+         int callDivert = input.nextInt();
+
+    switch (callDivert){
+    case 7-> System.out.println("""
+    1. Call divert
+
+""");   
+    }
+
+
+
+//    System.out.println("Choose an option");
+         int games = input.nextInt();
+
+    switch (games){
+    case 8-> System.out.println("""
+    1. Games
+""");   
+    }
+
+
+//    System.out.println("Choose an option");
+         int calculator = input.nextInt();
+
+    switch (calculator){
+    case 9-> System.out.println("""
+    1. Calculator
+""");   
+    }
+
+
+//    System.out.println("Choose an option");
+         int reminder = input.nextInt();
+
+    switch (reminder){
+    case 10-> System.out.println("""
+    1. reminder
+""");   
+    }
+
+
+//    System.out.println("Choose an option");
+         int clock = input.nextInt();
+
+    switch (clock){
+    case 11-> System.out.println("""
+    1. Alarm clock
+    2. Clock settings
+    3. Date setting
+    4. Stopwatch
+    5. Countdown time
+    6. Auto update of date and time
+""");   
+    }
+
+
+//    System.out.println("Choose an option");
+         int profiles = input.nextInt();
+
+    switch (profiles){
+    case 12-> System.out.println("""
+    1. Profiles
+""");   
+    }
+
+//   System.out.println("Choose an option");
+         int simService = input.nextInt();
+
+    switch (simService){
+    case 13-> System.out.println("""
+    1. SIM services
+""");   
+    }
 
 }
 
